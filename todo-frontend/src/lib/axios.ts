19b99a2e8b2ccba4ce69f326b2c7d9abe641f0b3
Poorna Madusith <157389469+poorna-madusith@users.noreply.github.com>
2025-09-06 12:43:00 +0530

    created custom axios instance

---
+++ todo-frontend/src/lib/axios.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { getAuth } from "firebase/auth";
+
+const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+});
+
+api.interceptors.request.use(async (config) => {
+  const auth = getAuth();
+  const user = auth.currentUser;
+  if (user) {
+    const token = await user.getIdToken();
+    config.headers.Authorization = `Bearer  ${token}`;
+  }
+
+  return config;
+});
+
+export default api;
